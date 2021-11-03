384461ff4442a3f579e51130aa630ff6b6d95fc5
raphaela <raphaelabaldi12345@gmail.com>
2021-11-03 13:21:19 -0300

    feat: draft to signup button with modal

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,10 +1,28 @@
 import React, {useState, useEffect} from 'react'
-import './App.css';
+import Modal from '@mui/material/Modal';
 import Post from './Post';
+import Button from '@mui/material/Button';
 import { db } from './firebase';
+import './App.css';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [posts, setPosts] = useState([]); 
+  const [open, setOpen] = useState(false);
 
   useEffect(() =>{
     db.collection('posts').onSnapshot(snapshot => {
@@ -17,7 +35,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1> Hello Instagram! </h1>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={style}>
+          <h2> This is a modal </h2>
+        </div>
+      </Modal>
 
       <div className = "app__header">
           <img
@@ -26,6 +51,8 @@ function App() {
             alt=""
           />
       </div>
+
+      <Button onClick={handleOpen}> SignUp </Button>
 
       {
         posts.map(({id, post}) => (
